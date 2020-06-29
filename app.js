1ea32c83e13c9e934e1f03e9fea472427d623ee0
Arianna Basha <ariannagbasha@gmail.com>
2020-06-29 14:35:17 -0500

    Error coding for uploads

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 import express from 'express';
 import multer from 'multer';
 import ejs from 'ejs';
+import fs from 'fs'
 import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + 
+        path.extname(file.originalname));
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 10000000},
+    fileFitler: function(req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('myImage');
 
 const app = express();
 
 app.set('view engine', 'ejs')
 
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/; 
+    //Check ext
+    const extname = filetypes.test(path.extname 
+        (file.originalname.toLowerCase()));
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype)
+
+    if(mimetype && extname) {
+        return cb(null, true);
+    } else {
+        return cb('Error: Images Only!');
+    }
+}
+
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.render('index'))
+
+app.post('/upload', (req,res) => {
+    upload(req, res, err => {
+        if(err) {
+            res.render('index', {
+                msg: err
+            });
+        } else {
+            console.log(req.file);
+            res.send('test');
+        }
+    })
+})
 
 const port = 3000;
 
